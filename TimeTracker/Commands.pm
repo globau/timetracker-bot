bc8b9ef4d0664c162307925bb98d7b71b10008b8
byron jones <byron@glob.com.au>
2014-08-20 10:01:33 -0400

    bad commands shouldn't crash the bot

--- TimeTracker/Commands.pm
+++ TimeTracker/Commands.pm
@@ -6,6 +6,7 @@ use File::Spec;
 use Module::Refresh;
 
 has _handler_files => ( is => 'rw', default => sub { [] } );
+has _handlers      => ( is => 'rw' );
 has _refresher     => ( is => 'lazy' );
 
 sub refresh_handlers {
@@ -14,10 +15,7 @@ sub refresh_handlers {
     foreach my $file (@{ $self->_handler_files }) {
         $self->_refresher->refresh_module($file);
     }
-}
 
-sub handlers {
-    my ($self) = @_;
     my $handlers = [];
 
     my (undef, $dir) = File::Spec->splitpath(__FILE__);
@@ -26,15 +24,27 @@ sub handlers {
         next if $class eq 'Base.pm';
         push @{ $self->_handler_files }, $file
             unless grep { $_ eq $file } @{ $self->_handler_files };
-        require $file;
 
-        $class =~ s/\.pm$//;
-        $class = "TimeTracker::Command::$class";
-        push @$handlers, $class->new();
-        my $object = $class->new();
+        eval {
+            require $file;
+            $class =~ s/\.pm$//;
+            $class = "TimeTracker::Command::$class";
+            push @$handlers, $class->new();
+        };
+        print STDERR "$@\n" if $@;
     }
 
     $handlers = [ sort { $a->triggers->[0] cmp $b->triggers->[0] } @$handlers ];
+    $self->_handlers($handlers);
+}
+
+sub handlers {
+    my ($self) = @_;
+    my $handlers = $self->_handlers;
+    if (!$handlers) {
+        $self->refresh_handlers();
+        $handlers = $self->_handlers;
+    }
     return $handlers;
 }
 
